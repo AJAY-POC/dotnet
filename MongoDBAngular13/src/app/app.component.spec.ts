dd3ab380eb79d8557bef183f6c6005b9169728f8
sriAjaygit <126223204+sriAjaygit@users.noreply.github.com>
2024-07-24 16:09:59 +0530

    Update app.component.spec.ts

--- MongoDBAngular13/src/app/app.component.spec.ts
+++ MongoDBAngular13/src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'MongoDBAngular13'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MongoDBAngular13');
+    expect(app.title).toEqual('DB@MongoDBAngular13');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Motivity Labs DevOps POC');
+    expect(compiled.querySelector('h1')?.textContent).toContain('welcome');
   });
 });
